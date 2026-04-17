430a6d96af87d6479d9da82c577db925b90aab9b
niferreira <nicolas.ferreira64680@live.fr>
2026-04-17 23:19:42 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('bebe-bearn-2');
+export class App implements OnInit {
+  protected readonly title = signal('Le futur bébé');
+
+  protected remainingTime = signal<Date | null>(null);
+
+  protected remainingHoursSeconde = computed(() =>
+    this.remainingTime()?.toISOString().slice(11, 19),
+  );
+
+  protected isTimePassed = signal<boolean>(false);
+
+  private eventTime = new Date(2026, 3, 18, 14, 0, 0, 0);
+
+  ngOnInit(): void {
+    timer(0, 1000).subscribe(() => {
+      const remainingTime = this.eventTime.getTime() - new Date().getTime();
+
+      if (remainingTime < 0) {
+        this.isTimePassed.set(true);
+      } else {
+        this.isTimePassed.set(false);
+      }
+
+      this.remainingTime.set(new Date(remainingTime));
+    });
+  }
 }
